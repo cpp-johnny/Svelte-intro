68d3c2a243d31046edac00bb88b5484ff3f40d55
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-08 22:54:24 +0800

    Create 04-nested-components.svelte

---
+++ src/Part-1/Introduction/04-nested-components.svelte
@@ -0,0 +1,43 @@
+<!--- new in src/app.svelte--->
+
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<!--- new in src/Nested.svelte --->
+<p>This is another paragraph.</p>
+
+<!---
+
+It would be impractical to put your entire app in a single component. Instead, we can import components from other files and include them in our markup.
+
+Add a <script> tag that imports Nested.svelte...
+
+--->
+
+<!--- in src/app.svelte --->
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
+<p>This is a paragraph.</p>
+<Nested />
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+
+<!--- in src/Nested.svelte --->
+<p>This is another paragraph.</p>
+
