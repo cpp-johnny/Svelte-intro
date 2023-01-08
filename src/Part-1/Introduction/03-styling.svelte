2b15a917d42da3148a30482d7366af0112be263d
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-08 22:49:08 +0800

    Create 03-styling.svelte

---
+++ src/Part-1/Introduction/03-styling.svelte
@@ -0,0 +1,23 @@
+<!--- new --->
+
+<p>This is a paragraph.</p>
+
+<style>
+	/* Write your CSS here */
+</style>
+
+
+<!---
+Just like in HTML, you can add a <style> tag to your component. Let's add some styles to the <p> element
+--->
+
+
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
