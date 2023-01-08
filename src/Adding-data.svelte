092dc928b25231168f586bae098718876f86e528
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-08 22:34:13 +0800

    Create Adding-data.svelte

---
+++ src/Adding-data.svelte
@@ -0,0 +1,33 @@
+<!--
+Adding data
+A component that just renders some static markup isn't very interesting. Let's add some data.
+
+First, add a script tag to your component and declare a name variable:
+-->
+
+
+<!--- this outputs: Hello World!--->
+
+<h1>Hello World!</h1>
+
+
+<!--- this outputs: Hello Svelte! --->
+<script>
+  let name = 'Svelte';
+</script>
+
+<h1>Hello {name}!</h1>
+
+
+<!--- this outputs: Hello SVELTE! --->
+<!--- 
+Inside the curly braces, we can put any JavaScript we want. 
+Try changing name to name.toUpperCase() for a shoutier greeting.
+--->
+
+<script>
+  let name = 'Svelte';
+</script>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
