5895ee086d610f14463f1be195badba2b66de058
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-08 23:09:46 +0800

    Update 01-assignments.svelte

--- src/Part-1/02-Reactivity/01-assignments.svelte
+++ src/Part-1/02-Reactivity/01-assignments.svelte
@@ -1,1 +1,39 @@
+<!--- new --->
+
+<script>
+	let count = 0;
+
+	function increment() {
+		// event handler code goes here
+	}
+</script>
+
+<button>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+
+<!--- 
+
+At the heart of Svelte is a powerful system of reactivity for keeping the DOM in sync with your application state — for example, in response to an event.
+
+To demonstrate it, we first need to wire up an event handler (we'll learn more about these later)
+
+Inside the increment function, all we need to do is change the value of count
+--->
+
+<!--- solution --->
+<script>
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
 
